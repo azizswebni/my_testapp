1bed5ef72106f0261b0ec224ce0cdd2e7c03b79f
Aziz Souabni <swabni55@gmail.com>
2022-03-01 15:39:32 +0100

    1st

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:my_testapp/providers/client_state_provider.dart';
+import 'package:my_testapp/providers/game_state_provider.dart';
+import 'package:my_testapp/screens/create_room_screen.dart';
+import 'package:my_testapp/screens/game_screen.dart';
+import 'package:my_testapp/screens/home_screen.dart';
+import 'package:my_testapp/screens/join_room_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GameStateProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ClientStateProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Typeracer Tutorial',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomeScreen(),
+          '/create-room': (context) => const CreateRoomScreen(),
+          '/join-room': (context) => const JoinRoomScreen(),
+          '/game-screen': (context) => const GameScreen(),
+        },
+      ),
+    );
+  }
+}
